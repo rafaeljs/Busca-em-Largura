2a48aa59e9e73986e5efb3da2ace61d6124ef67d
Rafael <rafaeljsilverio@hotmail.com>
2016-10-24 19:44:09 -0300

    alteraçao

--- ConsoleApplication3/Grafo.cs
+++ ConsoleApplication3/Grafo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication3 {
+namespace Busca_em_Lagura {
     class Grafo {
         List<int>[] Grafos;
         int[] Fila;
--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using Busca_em_Lagura;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication3
-{
+namespace Busca_em_Largura{
     class Program{
         static void Main(string[] args){
             int quantidade;
